9f835044aef0c4aab0fbd6419d6e31f9189035d3
Shivam Raj <shivamraj@AS-MAC-0208.local>
2022-03-29 12:41:13 +0530

    Kubernetes Files Added

--- dags/execution_dag.py
+++ dags/execution_dag.py
@@ -2,7 +2,25 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.dummy_operator import DummyOperator
 from datetime import datetime
-from utils import fetch_and_store_execution_dag_run
+# from utils import fetch_and_store_execution_dag_run
+
+import psycopg2
+
+
+def fetch_and_store_execution_dag_run():
+    try:
+        conn = psycopg2.connect(host="postgres", database="airflow", user="airflow", password="airflow", port='5432')
+        cursor = conn.cursor()
+        drop_table = """drop table IF EXISTS execution_table"""
+        cursor.execute(drop_table)
+        conn.commit()
+        create_query = """create table execution_table as select dag_id, execution_date from dag_run order by execution_date;"""
+        cursor.execute(create_query)
+        conn.commit()
+    except Exception as e:
+        print(e)
+    finally:
+        conn.close()
 
 default_args = {
     "owner": "airflow",
